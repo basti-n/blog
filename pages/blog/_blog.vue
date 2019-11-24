2c2919beee38c89b3b7bcd99432a776374049b8e
Sebastian Neumair <sebastianneumair@Sebastians-MBP.fritz.box>
2019-11-24 18:39:20 +0100

    Add Blog Component

---
+++ pages/blog/_blog.vue
@@ -0,0 +1,20 @@
+<template>
+  <article>
+    <h2>{{ blogPost.title }}</h2>
+    <div>
+      {{blogPost.body }}
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, payload }) {
+    if (payload) {
+      return { blogPost: payload };
+    } else {
+      return await require(`~/assets/content/blog/${params.blogPost}.json`);
+    }
+  }
+};
+</script>
